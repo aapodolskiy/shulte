136ebeaa5f7d89d0fe6e0c3097e5fc977f080271
Alexander <a.a.podolskiy@gmail.com>
2018-08-08 00:11:45 +0300

    initial commit

---
+++ src/main/java/com/example/alexander/shulte02/MainActivity.kt
@@ -0,0 +1,71 @@
+package com.example.alexander.shulte02
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.Date
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var randomizedArray = (1..25).shuffled()
+
+        val cells = listOf(
+                view_s01,
+                view_s02,
+                view_s03,
+                view_s04,
+                view_s05,
+                view_s06,
+                view_s07,
+                view_s08,
+                view_s09,
+                view_s10,
+                view_s11,
+                view_s12,
+                view_s13,
+                view_s14,
+                view_s15,
+                view_s16,
+                view_s17,
+                view_s18,
+                view_s19,
+                view_s20,
+                view_s21,
+                view_s22,
+                view_s23,
+                view_s24,
+                view_s25
+        )
+
+        val startTime = Date().time
+
+        var nextNumber = 1
+
+        currentNumberToFind.text = nextNumber.toString()
+
+        for (i in 0..24) {
+            cells[i].text = randomizedArray[i].toString()
+            cells[i].setOnClickListener{
+                if (nextNumber == randomizedArray[i]) {
+                    nextNumber++
+                    if (nextNumber == 26) {
+                        val finishTime = Date().time
+                        val totalTime = (finishTime - startTime)/1000.0
+                        val winText = "you win, your time is $totalTime seconds"
+                        currentNumberToFind.text = winText
+                    }
+                    else {
+                        currentNumberToFind.text = nextNumber.toString()
+                    }
+                }
+            }
+        }
+
+
+    }
+
+}
