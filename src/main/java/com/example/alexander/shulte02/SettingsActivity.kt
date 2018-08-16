28361177cb94d85a12899836ee3e82aaa2f772d0
Alexander <a.a.podolskiy@gmail.com>
2018-08-16 23:25:46 +0300

    preferences used

--- src/main/java/com/example/alexander/shulte02/SettingsActivity.kt
+++ src/main/java/com/example/alexander/shulte02/SettingsActivity.kt
@@ -1,5 +1,6 @@
 package com.example.alexander.shulte02
 
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_settings.*
 import android.os.Bundle
@@ -13,6 +14,38 @@ class SettingsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_settings)
+
+        val myPreferences = getSharedPreferences("myPreferences", Context.MODE_PRIVATE)
+
+        if (myPreferences.contains("timer")) {
+            timerSwitch.isChecked = myPreferences.getBoolean("timer", false)
+        }
+
+        if (myPreferences.contains("sound")) {
+            soundSwitch.isChecked = myPreferences.getBoolean("sound", false)
+        }
+
+        if (myPreferences.contains("other")) {
+            otherSwitch.isChecked = myPreferences.getBoolean("other", false)
+        }
+
+        timerSwitch.setOnClickListener{
+            val editor = myPreferences.edit()
+            editor.putBoolean("timer", timerSwitch.isChecked)
+            editor.apply()
+        }
+
+        soundSwitch.setOnClickListener{
+            val editor = myPreferences.edit()
+            editor.putBoolean("sound", soundSwitch.isChecked)
+            editor.apply()
+        }
+
+        otherSwitch.setOnClickListener{
+            val editor = myPreferences.edit()
+            editor.putBoolean("other", otherSwitch.isChecked)
+            editor.apply()
+        }
 
         settingsActivityMenuButton.setOnClickListener{
             val mainActivity = Intent(this, MainActivity::class.java)
