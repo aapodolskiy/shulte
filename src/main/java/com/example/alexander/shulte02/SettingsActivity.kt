91d79256688f6b6059c900b353f774a5e3094d2f
Alexander <a.a.podolskiy@gmail.com>
2018-08-16 23:51:35 +0300

    preferences work

--- src/main/java/com/example/alexander/shulte02/SettingsActivity.kt
+++ src/main/java/com/example/alexander/shulte02/SettingsActivity.kt
@@ -17,16 +17,37 @@ class SettingsActivity : AppCompatActivity() {
 
         val myPreferences = getSharedPreferences("myPreferences", Context.MODE_PRIVATE)
 
+        val timerSwitchDefault = false
         if (myPreferences.contains("timer")) {
-            timerSwitch.isChecked = myPreferences.getBoolean("timer", false)
+            timerSwitch.isChecked = myPreferences.getBoolean("timer", timerSwitchDefault)
+        }
+        else {
+            val editor = myPreferences.edit()
+            editor.putBoolean("timer", timerSwitchDefault)
+            editor.apply()
+            timerSwitch.isChecked = timerSwitchDefault
         }
 
+        val soundSwitchDefault = false
         if (myPreferences.contains("sound")) {
-            soundSwitch.isChecked = myPreferences.getBoolean("sound", false)
+            soundSwitch.isChecked = myPreferences.getBoolean("sound", soundSwitchDefault)
+        }
+        else {
+            val editor = myPreferences.edit()
+            editor.putBoolean("sound", soundSwitchDefault)
+            editor.apply()
+            soundSwitch.isChecked = soundSwitchDefault
         }
 
+        val otherSwitchDefault = true
         if (myPreferences.contains("other")) {
-            otherSwitch.isChecked = myPreferences.getBoolean("other", false)
+            otherSwitch.isChecked = myPreferences.getBoolean("other", otherSwitchDefault)
+        }
+        else {
+            val editor = myPreferences.edit()
+            editor.putBoolean("other", otherSwitchDefault)
+            editor.apply()
+            otherSwitch.isChecked = otherSwitchDefault
         }
 
         timerSwitch.setOnClickListener{
